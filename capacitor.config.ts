4387d4a5dec78b0a50d4e5e1ea887ba204e7da09
Jorge Sandoval <82074301+jorsandoval@users.noreply.github.com>
2022-08-31 23:52:51 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PGY4121_EVA01',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
